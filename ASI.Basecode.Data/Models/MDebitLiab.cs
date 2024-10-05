51c208272ba491f3a0cb176cd8638b9aa32ac5d7
CLAB3-25\db2 <jamesniza1@gmail.com>
2024-10-05 16:01:21 +0800

    Models Added through Scaffolding

---
+++ ASI.Basecode.Data/Models/MDebitLiab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASI.Basecode.Data.Models
+{
+    public partial class MDebitLiab
+    {
+        public MDebitLiab()
+        {
+            MTransactions = new HashSet<MTransaction>();
+        }
+
+        public int DeLiId { get; set; }
+        public string DeLiType { get; set; }
+        public decimal DeLiBalance { get; set; }
+        public string DeLiIcon { get; set; }
+        public string DeLiColor { get; set; }
+        public string DeLiDest { get; set; }
+        public DateTime? DeLiHapp { get; set; }
+        public DateTime? DeLiDue { get; set; }
+
+        public virtual ICollection<MTransaction> MTransactions { get; set; }
+    }
+}
