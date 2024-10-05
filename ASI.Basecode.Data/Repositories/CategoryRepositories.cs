736e8858a5e33133c0ff06ec60550cc25774f1d4
James Anthony S. Ceniza <jamesniza1@gmail.com>
2024-10-06 03:54:08 +0800

    Improve Crud Implementation

---
+++ ASI.Basecode.Data/Repositories/CategoryRepositories.cs
@@ -0,0 +1,51 @@
+﻿using ASI.Basecode.Data.Interfaces;
+using ASI.Basecode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASI.Basecode.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly List<MCategory> _categories = new List<MCategory>();
+        private int _nextId = 1;
+
+        public IEnumerable<MCategory> GetCategoriesAsync()
+        {
+            return _categories;
+        }
+
+        public MCategory GetCategoryByIdAsync(int id)
+        {
+            return _categories.FirstOrDefault(c => c.CategoryId == id);
+        }
+
+        public void AddCategoryAsync(MCategory category)
+        {
+            category.CategoryId = _nextId++;
+            _categories.Add(category);
+        }
+
+        public void UpdateCategoryAsync(MCategory category)
+        {
+            var existingCategory = _categories.FirstOrDefault(c => c.CategoryId == category.CategoryId);
+            if (existingCategory != null)
+            {
+                existingCategory.Name = category.Name;
+                existingCategory.Icon = category.Icon;
+                existingCategory.Color = category.Color;
+                existingCategory.Type = category.Type;
+            }
+        }
+
+        public void DeleteCategoryAsync(int id)
+        {
+            var category = _categories.FirstOrDefault(c => c.CategoryId == id);
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+    }
+}
---
+++ ASI.Basecode.Services/Services/CategoryService.cs
@@ -0,0 +1,67 @@
+﻿using ASI.Basecode.Data;
+using ASI.Basecode.Data.Models;
+using Microsoft.EntityFrameworkCore; // Ensure this is included
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.WebApp.Services
+{
+    public class CategoryService
+    {
+        private readonly AsiBasecodeDbContext _context;
+
+        public CategoryService(AsiBasecodeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<MCategory>> GetCategoriesAsync()
+        {
+            return await _context.MCategories.ToListAsync(); // ToListAsync requires Microsoft.EntityFrameworkCore
+        }
+
+        public async Task AddCategoryAsync(MCategory category)
+        {
+            _context.MCategories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<MCategory> GetCategoryByIdAsync(int id)
+        {
+            return await _context.MCategories.AsNoTracking() // AsNoTracking requires Microsoft.EntityFrameworkCore
+                                .FirstOrDefaultAsync(c => c.CategoryId == id);
+        }
+
+        public async Task UpdateCategoryAsync(MCategory category)
+        {
+            var existingCategory = await _context.MCategories.FindAsync(category.CategoryId);
+            if (existingCategory != null)
+            {
+                existingCategory.Name = category.Name;
+                existingCategory.Type = category.Type;
+                existingCategory.Icon = category.Icon;
+                existingCategory.Color = category.Color;
+
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Category with ID {category.CategoryId} not found.");
+            }
+        }
+
+        public async Task DeleteCategoryAsync(int id)
+        {
+            var category = await _context.MCategories.FindAsync(id);
+            if (category != null)
+            {
+                _context.MCategories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Category with ID {id} not found.");
+            }
+        }
+    }
+}
