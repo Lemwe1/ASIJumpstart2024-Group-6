eea7b51acd9a9ebef8bc00e0599b7dc70a24c2b7
Lemuel <lemueldioquino@gmail.com>
2024-11-10 03:52:05 +0800

    most pages now has darkmode including nav,transaction, wallet, category

--- ASI.Basecode.WebApp/wwwroot/js/site.js
+++ ASI.Basecode.WebApp/wwwroot/js/site.js
@@ -1,17 +1,17 @@
 ﻿// Logout confirmation dialog
-        document.getElementById('logoutLink').addEventListener('click', function (event) {
-            event.preventDefault();
+document.getElementById('logoutLink').addEventListener('click', function (event) {
+    event.preventDefault();
 
-            Swal.fire({
-                text: "Are you sure you want to log out?",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Logout'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = this.getAttribute('href');
-                }
-            });
-        });
+    Swal.fire({
+        text: "Are you sure you want to log out?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#a6a6a6',
+        confirmButtonText: 'Logout'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = this.getAttribute('href');
+        }
+    });
+});
